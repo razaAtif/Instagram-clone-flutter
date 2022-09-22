b77e526a2006e8937c6164cd9d6793af3d3b79ba
atif-git <atifraza1430@gmail.com>
2022-09-22 10:41:25 +0530

    step-7

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/sigup_screen.dart';
 
@@ -33,36 +35,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Instagram clone',
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: mobileBackgroundColor,
-        ),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              if (snapshot.hasData) {
-                return const ResponsiveLayout(
-                  mobileScreenLayout: MobileScreeLayout(),
-                  webScreenLayout: WebScreeLayout(),
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('${snapshot.error}'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        )
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Instagram clone',
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: mobileBackgroundColor,
+          ),
+          home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  return const ResponsiveLayout(
+                    mobileScreenLayout: MobileScreeLayout(),
+                    webScreenLayout: WebScreeLayout(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('${snapshot.error}'),
+                  );
+                }
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: primaryColor,
+                  ),
                 );
               }
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: primaryColor,
-                ),
-              );
-            }
-            return const LogInScreen();
-          },
-        ));
+              return const LogInScreen();
+            },
+          )),
+    );
   }
 }
--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:instagram_flutter/models/user.dart' as model;
 
-class MobileScreeLayout extends StatelessWidget {
+import '../providers/user_provider.dart';
+
+class MobileScreeLayout extends StatefulWidget {
   const MobileScreeLayout({Key? key}) : super(key: key);
 
   @override
+  State<MobileScreeLayout> createState() => _MobileScreeLayoutState();
+}
+
+class _MobileScreeLayoutState extends State<MobileScreeLayout> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    model.User user = Provider.of<UserProvider>(context).getUser;
+    return Scaffold(
       body: Center(
-        child: Text('This is Mobile'),
+        child: Text(user.email),
       ),
     );
   }
